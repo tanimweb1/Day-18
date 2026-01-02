88a26ddfceb6498cf59ce7a55313a4f226a1fdec
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-03 02:47:45 +0600

    Add files via upload

---
+++ Day-18.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main(){
+
+    //losagu and gosagu
+     
+int num1,num2,n1,n2,gcd,lcd,rem;
+printf("enter 2 number : ");
+scanf("%d%d",&num1,&num2);
+
+n1= num1;
+n2=num2;
+
+while (n2!=0)
+{
+   rem = n1%n2;
+   n1=n2;
+   n2=rem;
+}
+
+gcd = n1;
+lcd= num1*num2/gcd;
+printf("GCD is %d\n",gcd);
+printf("LCD is %d\n",lcd);
+
+
+
+
+
+
+
+
+    // sum of digits
+int number,sum,temp,rem;
+sum = 0;
+printf("Enter the number : ");
+scanf("%d", &number);
+
+temp = number;
+
+while (temp!=0)
+{
+    rem = temp%10;
+    sum = sum + rem;
+    temp = temp/10;
+}
+printf("Sum of digits is %d", sum);
+
+
+
+
+
+
+    return 0;
+}
